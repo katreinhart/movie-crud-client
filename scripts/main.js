2ab1c30e3a8f51c7df391fa5d565908f56f8cb3b
Kat Reinhart <kat.reinhart@gmail.com>
2017-11-06 12:00:38 -0800

    Create form for post/update

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,6 @@
-console.log('sanity check!');
+const localhostURL = 'http://localhost:3000/movies'
+const herokuURL = ''
+const baseURL = window.location.href.includes('localhost') ? localhostURL : herokuURL
+
+document.getElementById('main-content').innerHTML = formTemplate('POST')
+
