ac666a06358c84f4ed29b6eb6e2e5085c84bdc42
Kat Reinhart <kat.reinhart@gmail.com>
2017-11-06 16:36:59 -0800

    Clean up index.js and change li to a

--- scripts/templates/index.js
+++ scripts/templates/index.js
@@ -1,14 +1,12 @@
 const indexTemplate = (movies) => {
-  console.log(Array.isArray(movies))
   const ul = document.createElement('UL')
   ul.classList += 'list-group'
   movies.forEach(movie => {
-    const li = document.createElement('LI')
-    li.classList += 'list-group-item'
-    li.textContent = movie.title
-    console.log(li)
-    ul.appendChild(li)
+    const a = document.createElement('a')
+    a.classList += 'list-group-item'
+    a.textContent = movie.title
+    a.href = `#/${movie.id}`
+    ul.appendChild(a)
   })
-  console.log(ul)
   return ul
 }
