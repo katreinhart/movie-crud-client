8818f322c0ee7215e034885d19ed2c183436dd47
Kat Reinhart <kat.reinhart@gmail.com>
2017-11-07 13:37:21 -0800

    Frontend styling

--- scripts/home.js
+++ scripts/home.js
@@ -2,7 +2,7 @@ navBarView.homePageInit()
 
 document.getElementById('main-content').innerHTML = `
   <div>
-    <h1>Kat's Sci-Fi Movie Database</h1>
+    <h1 class="page-title">Kat's Sci-Fi Movie Database</h1>
     <h4><a href="/movies/#">Enter</a></h4>
   </div>
 `
